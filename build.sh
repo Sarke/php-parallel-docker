846ca240ff8d82373c075b9d0857b5e74d825548
Peter Stalman <sarkedev@gmail.com>
2021-03-06 23:32:07 -0800

    always test

--- build.sh
+++ build.sh
@@ -10,16 +10,19 @@ docker-php-source extract
 docker-php-ext-configure $EXT
 
 if [ $1 = "test" ]; then
-	make test
+	printf 'n\n' | make test
+else
+	printf 'n\n' | make test || true
 fi
 
 docker-php-ext-install $EXT
 
-docker-php-source delete
+#docker-php-source delete
 
 set +x
 EXTENSION_DIR=`php-config --extension-dir 2>/dev/null`
 
-echo "===== Built ====="
-sha256sum "$EXTENSION_DIR/$EXT.so"
-echo "================="
+echo "====================================================================="
+echo "Finished building"
+sha256sum "$EXTENSION_DIR/$EXT.so" | sed "s/  /\n/"
+echo "====================================================================="
